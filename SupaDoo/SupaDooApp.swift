c281edfb3fea5fcecbe81a33ccf52ef0c00de595
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-11-14 23:16:50 +0100

    Initial Commit

---
+++ SupaDoo/SupaDooApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaDooApp.swift
+//  SupaDoo
+//
+//  Created by Nicolò Curioni on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SupaDooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
